bce5346ae1bd8b03810bc359e15f474b0016006f
SeangLeng <seangleng507@gmail.com>
2023-04-19 14:22:41 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { RenderingCards } from './components/RenderingCards';
+
+function App() {
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <RenderingCards/>  
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
